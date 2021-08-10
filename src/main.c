921af5e2898c60147d043aa938680a3fa1080d33
Chengshusss <chengshu_seu@163.com>
2021-08-10 22:21:15 +0800

    fix test problem

--- src/main.c
+++ src/main.c
@@ -16,8 +16,8 @@
 typedef struct
 {
     u_int32_t id;
-    char username[COLUMN_EMAIL_SIZE];
-    char email[COLUMN_USERNAME_SIZE];
+    char username[COLUMN_USERNAME_SIZE + 1];
+    char email[COLUMN_EMAIL_SIZE + 1];
 } Row;
 const u_int32_t ID_SIZE = size_of_attribute(Row, id);
 const u_int32_t USERNAME_SIZE = size_of_attribute(Row, username);
@@ -25,7 +25,7 @@ const u_int32_t EMAIL_SIZE = size_of_attribute(Row, email);
 const u_int32_t ID_OFFSET = 0;
 const u_int32_t USERNAME_OFFSET = ID_OFFSET + ID_SIZE;
 const u_int32_t EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE;
-const u_int32_t ROW_SIZE = ID_SIZE + USERNAME_SIZE + EMAIL_OFFSET;
+const u_int32_t ROW_SIZE = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE;
 const u_int32_t PAGE_SIZE = 4096;
 const u_int32_t ROWS_PER_PAGE = PAGE_SIZE / ROW_SIZE;
 const u_int32_t TABLE_MAX_ROWS = ROWS_PER_PAGE * TABLE_MAX_PAGES;
